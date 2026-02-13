91eae12fb0cd6b9bb5570ca8c938c6721591d0e9
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2026-02-13 18:21:21 +0200

    SmartResource | Added RAII wrapper over C resources. Specialization for SDL.

---
+++ modules/SmartResource/include/SmartResource/SmartResource.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+namespace lbr::smartresource
+{
+template <typename Resource, void CleanupFunction(Resource *)>
+struct SmartResource
+{
+    /**
+     * \brief Use direct access at your own risk.
+     */
+    Resource *res {nullptr};
+
+    SmartResource() = default;
+    explicit SmartResource(Resource *res) noexcept;
+
+    SmartResource(const SmartResource &) = delete;
+    SmartResource &operator=(const SmartResource &) = delete;
+
+    SmartResource(SmartResource &&other) noexcept;
+    SmartResource &operator=(SmartResource &&other) noexcept;
+
+    ~SmartResource() noexcept;
+
+    Resource *release() noexcept;
+};
+
+} // namespace lbr::smartresource
+
+#include "detail/SmartResource.hxx"
---
+++ modules/SmartResource/include/SmartResource/detail/SmartResource.hxx
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "../SmartResource.hpp"
+
+namespace lbr::smartresource
+{
+template <typename Resource, void CleanupFunction(Resource *)>
+SmartResource<Resource, CleanupFunction>::SmartResource(Resource *res) noexcept : res {res}
+{
+}
+
+template <typename Resource, void CleanupFunction(Resource *)>
+SmartResource<Resource, CleanupFunction>::SmartResource(SmartResource &&other) noexcept
+{
+    this->res = other.res;
+    other.res = nullptr;
+};
+
+template <typename Resource, void CleanupFunction(Resource *)>
+SmartResource<Resource, CleanupFunction> &
+    SmartResource<Resource, CleanupFunction>::operator=(SmartResource &&other) noexcept
+{
+    if (this != &other)
+    {
+        this->res = other.res;
+        other.res = nullptr;
+    }
+    return *this;
+}
+
+template <typename Resource, void CleanupFunction(Resource *)>
+SmartResource<Resource, CleanupFunction>::~SmartResource() noexcept
+{
+    if (res != nullptr)
+        CleanupFunction(res);
+}
+
+template <typename Resource, void CleanupFunction(Resource *)>
+Resource *SmartResource<Resource, CleanupFunction>::release() noexcept
+{
+    auto moved = res;
+    res = nullptr;
+    return moved;
+}
+
+} // namespace lbr::smartresource
